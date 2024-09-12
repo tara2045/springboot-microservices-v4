e43b7de9c8db255cdf17b48362886e8595ada8f1
tara2045 <45599450+tara2045@users.noreply.github.com>
2024-09-12 16:10:48 -0500

    update @RequestMapping to @GetMapping
    update @RequestMapping to @GetMapping

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	
 	// /hello-world
+	// plz update @RequestMapping to @GetMapping
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/hello-world1")
 	public String helloWorld() {
